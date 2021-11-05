255b4cdd98b044e56e2717b32da35e28a0c36368
kulasekhar <thurlukulasekhar@gmail.com>
2021-11-05 19:48:44 +0530

    05/112021

---
+++ src/main/java/com/app/controller/EmpController.java
@@ -0,0 +1,17 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+public class EmpController {
+
+    // get emp by id
+    @RequestMapping("/getEmpById")
+    
+    public String getEmpById(int id) {
+     
+
+
+}
